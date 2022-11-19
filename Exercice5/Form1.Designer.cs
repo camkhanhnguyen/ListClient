5ed15d0da74108fbc1f5f48729b5557932a5a6cf
nguyen-khanh-github <camkhanhpro@gmail.com>
2022-11-19 23:20:51 +0100

    Ajoutez des fichiers projet.

---
+++ Exercice5/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿
+namespace Exercice5
+{
+    partial class frmBeautyShop
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.lblLogin = new System.Windows.Forms.Label();
+            this.lblBesoins = new System.Windows.Forms.Label();
+            this.cboBesoins = new System.Windows.Forms.ComboBox();
+            this.lblMarques = new System.Windows.Forms.Label();
+            this.cboMarques = new System.Windows.Forms.ComboBox();
+            this.lblQuantités = new System.Windows.Forms.Label();
+            this.nudQuantités = new System.Windows.Forms.NumericUpDown();
+            this.lstClients = new System.Windows.Forms.ListBox();
+            this.btnAjouter = new System.Windows.Forms.Button();
+            this.rdbSoinVisage = new System.Windows.Forms.RadioButton();
+            this.rdbMaquillage = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.nudQuantités)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLogin.Location = new System.Drawing.Point(127, 41);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(270, 27);
+            this.txtLogin.TabIndex = 0;
+            // 
+            // lblLogin
+            // 
+            this.lblLogin.AutoSize = true;
+            this.lblLogin.Location = new System.Drawing.Point(43, 47);
+            this.lblLogin.Name = "lblLogin";
+            this.lblLogin.Size = new System.Drawing.Size(43, 17);
+            this.lblLogin.TabIndex = 1;
+            this.lblLogin.Text = "Login";
+            // 
+            // lblBesoins
+            // 
+            this.lblBesoins.AutoSize = true;
+            this.lblBesoins.Location = new System.Drawing.Point(43, 102);
+            this.lblBesoins.Name = "lblBesoins";
+            this.lblBesoins.Size = new System.Drawing.Size(58, 17);
+            this.lblBesoins.TabIndex = 2;
+            this.lblBesoins.Text = "Besoins";
+            // 
+            // cboBesoins
+            // 
+            this.cboBesoins.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboBesoins.FormattingEnabled = true;
+            this.cboBesoins.Items.AddRange(new object[] {
+            "Soin anti-rides & anti-âge",
+            "Soin hydratant & nourrissant",
+            "Soin matifiant",
+            "Soin anti-imperfections",
+            "Soin anti-rougeurs",
+            "Soin peaux sensibles",
+            "Soin raffermissant & liftant",
+            "Soin anti tache",
+            "Soin éclat & anti-Fatigue"});
+            this.cboBesoins.Location = new System.Drawing.Point(127, 96);
+            this.cboBesoins.Name = "cboBesoins";
+            this.cboBesoins.Size = new System.Drawing.Size(270, 28);
+            this.cboBesoins.TabIndex = 5;
+            // 
+            // lblMarques
+            // 
+            this.lblMarques.AutoSize = true;
+            this.lblMarques.Location = new System.Drawing.Point(43, 156);
+            this.lblMarques.Name = "lblMarques";
+            this.lblMarques.Size = new System.Drawing.Size(63, 17);
+            this.lblMarques.TabIndex = 6;
+            this.lblMarques.Text = "Marques";
+            // 
+            // cboMarques
+            // 
+            this.cboMarques.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboMarques.FormattingEnabled = true;
+            this.cboMarques.Items.AddRange(new object[] {
+            "DIOR",
+            "GUERLAIN",
+            "CHANEL",
+            "TOO FACED",
+            "LANCÔME",
+            "YVES SAINT LAURENT",
+            "CLARINS",
+            "BENEFIT COSMETICS",
+            "NARS"});
+            this.cboMarques.Location = new System.Drawing.Point(127, 150);
+            this.cboMarques.Name = "cboMarques";
+            this.cboMarques.Size = new System.Drawing.Size(270, 28);
+            this.cboMarques.TabIndex = 7;
+            // 
+            // lblQuantités
+            // 
+            this.lblQuantités.AutoSize = true;
+            this.lblQuantités.Location = new System.Drawing.Point(43, 202);
+            this.lblQuantités.Name = "lblQuantités";
+            this.lblQuantités.Size = new System.Drawing.Size(75, 17);
+            this.lblQuantités.TabIndex = 8;
+            this.lblQuantités.Text = "Quantié(s)";
+            // 
+            // nudQuantités
+            // 
+            this.nudQuantités.Location = new System.Drawing.Point(127, 200);
+            this.nudQuantités.Name = "nudQuantités";
+            this.nudQuantités.Size = new System.Drawing.Size(70, 22);
+            this.nudQuantités.TabIndex = 9;
+            // 
+            // lstClients
+            // 
+            this.lstClients.FormattingEnabled = true;
+            this.lstClients.ItemHeight = 16;
+            this.lstClients.Location = new System.Drawing.Point(46, 251);
+            this.lstClients.Name = "lstClients";
+            this.lstClients.ScrollAlwaysVisible = true;
+            this.lstClients.Size = new System.Drawing.Size(505, 180);
+            this.lstClients.TabIndex = 10;
+            // 
+            // btnAjouter
+            // 
+            this.btnAjouter.Location = new System.Drawing.Point(447, 148);
+            this.btnAjouter.Name = "btnAjouter";
+            this.btnAjouter.Size = new System.Drawing.Size(104, 25);
+            this.btnAjouter.TabIndex = 11;
+            this.btnAjouter.Text = "Ajouter";
+            this.btnAjouter.UseVisualStyleBackColor = true;
+            this.btnAjouter.Click += new System.EventHandler(this.btnAjouter_Click);
+            // 
+            // rdbSoinVisage
+            // 
+            this.rdbSoinVisage.AutoSize = true;
+            this.rdbSoinVisage.Location = new System.Drawing.Point(447, 45);
+            this.rdbSoinVisage.Name = "rdbSoinVisage";
+            this.rdbSoinVisage.Size = new System.Drawing.Size(104, 21);
+            this.rdbSoinVisage.TabIndex = 12;
+            this.rdbSoinVisage.TabStop = true;
+            this.rdbSoinVisage.Text = "Soin Visage";
+            this.rdbSoinVisage.UseVisualStyleBackColor = true;
+            this.rdbSoinVisage.CheckedChanged += new System.EventHandler(this.rdbSoinVisage_CheckedChanged);
+            // 
+            // rdbMaquillage
+            // 
+            this.rdbMaquillage.AutoSize = true;
+            this.rdbMaquillage.Location = new System.Drawing.Point(447, 98);
+            this.rdbMaquillage.Name = "rdbMaquillage";
+            this.rdbMaquillage.Size = new System.Drawing.Size(97, 21);
+            this.rdbMaquillage.TabIndex = 13;
+            this.rdbMaquillage.TabStop = true;
+            this.rdbMaquillage.Text = "Maquillage";
+            this.rdbMaquillage.UseVisualStyleBackColor = true;
+            this.rdbMaquillage.CheckedChanged += new System.EventHandler(this.rdbMaquillage_CheckedChanged);
+            // 
+            // frmBeautyShop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(570, 450);
+            this.Controls.Add(this.rdbMaquillage);
+            this.Controls.Add(this.rdbSoinVisage);
+            this.Controls.Add(this.btnAjouter);
+            this.Controls.Add(this.lstClients);
+            this.Controls.Add(this.nudQuantités);
+            this.Controls.Add(this.lblQuantités);
+            this.Controls.Add(this.cboMarques);
+            this.Controls.Add(this.lblMarques);
+            this.Controls.Add(this.cboBesoins);
+            this.Controls.Add(this.lblBesoins);
+            this.Controls.Add(this.lblLogin);
+            this.Controls.Add(this.txtLogin);
+            this.Name = "frmBeautyShop";
+            this.Text = "Beauty Shop";
+            this.Load += new System.EventHandler(this.frmBeautyShop_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudQuantités)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtLogin;
+        private System.Windows.Forms.Label lblLogin;
+        private System.Windows.Forms.Label lblBesoins;
+        private System.Windows.Forms.ComboBox cboBesoins;
+        private System.Windows.Forms.Label lblMarques;
+        private System.Windows.Forms.ComboBox cboMarques;
+        private System.Windows.Forms.Label lblQuantités;
+        private System.Windows.Forms.NumericUpDown nudQuantités;
+        private System.Windows.Forms.ListBox lstClients;
+        private System.Windows.Forms.Button btnAjouter;
+        private System.Windows.Forms.RadioButton rdbSoinVisage;
+        private System.Windows.Forms.RadioButton rdbMaquillage;
+    }
+}
+
---
+++ Exercice5/Form1.cs
@@ -0,0 +1,242 @@
+﻿/**
+ * titre: BeautyShop projet
+ * description: ajouter la list client avec les beautés produits classifiés
+ * auteur: Khanh NGUYEN
+ * date: 19/11/2022
+ */
+using System;
+using System.Windows.Forms;
+
+namespace Exercice5
+{
+    public partial class frmBeautyShop : Form
+    {
+        /// <summary>
+        /// créer la classe mère Clients
+        /// </summary>
+        public abstract class Clients
+        {
+            // propriétés
+            protected string login = "inconnu";
+            protected int quantites;
+
+            /// <summary>
+            /// constructeur simple (inconnu)
+            /// </summary>
+            public Clients() { }
+
+            /// <summary>
+            /// constructeur de la class Clients
+            /// </summary>
+            /// <param name="login"></param>
+            public Clients(string login)
+            {
+                this.login = login;
+                
+            }
+
+            public int getQuantites()
+            {
+                return quantites;
+            }
+         
+            /// <summary>
+            /// 
+            /// </summary>
+            /// <returns></returns>
+            public abstract string info();
+
+        }
+
+        /// <summary>
+        /// créer la classe fille de la class Clients
+        /// </summary>
+        public class SoinVisage : Clients
+        {
+            // déclaration
+            string besoins;
+
+            /// <summary>
+            /// constructeur pour initialisation les besoins
+            /// </summary>
+            public SoinVisage(string besoins)
+            {
+                this.besoins = besoins;
+            }
+
+            /// <summary>
+            /// constructeur pour initialisation le login et les besoins
+            /// </summary>
+            /// <param name="login"></param>
+            /// <param name="besoins"></param>
+            public SoinVisage(string login, string besoins, int quantites): base(login)
+            {
+                this.besoins = besoins;
+                this.quantites = quantites;
+            }
+
+            /// <summary>
+            /// 
+            /// </summary>
+            /// <returns></returns>
+            public override string info()
+            {
+                return "Soin Visage : " + login + " (" + besoins + ", quantité(s) : " + quantites + ")";
+            }
+        }
+
+        /// <summary>
+        /// cr
+        /// </summary>
+        public class Maquillage : Clients
+        {
+            // déclaration
+            string marques;
+
+            /// <summary>
+            /// constructeur pour initialisation la marque
+            /// </summary>
+            /// <param name="marques"></param>
+            public Maquillage(string marques)
+            {
+                this.marques = marques;
+            }
+
+            /// <summary>
+            /// constructeur pour initialisation le login et la marque
+            /// </summary>
+            /// <param name="login"></param>
+            /// <param name="marques"></param>
+            public Maquillage(string login, string marques, int quantites): base (login)
+            {
+                this.marques = marques;
+                this.quantites = quantites;
+            }
+
+            /// <summary>
+            /// 
+            /// </summary>
+            /// <returns></returns>
+            public override string info()
+            {
+                return "Maquillage : " + login + " (" + marques + ", quantité(s) : " + quantites + ")";
+            }
+        }
+
+        /// <summary>
+        /// initialisation des components graphics
+        /// </summary>
+        public frmBeautyShop()
+        {
+            InitializeComponent();
+        }
+
+        // créer un tableau des clients
+        Clients[] lesClients = new Clients[100];
+        private int nbClient = 0;
+
+        /// <summary>
+        /// changement de la visible de la zone besoin et la zone marque
+        /// </summary>
+        /// <param name="visage"></param>
+        private void changeVisible(bool visage)
+        {
+            // visible les besoins et son combo box
+            lblBesoins.Visible = visage;
+            cboBesoins.Visible = visage;
+
+            // invisible les marques et son combo box
+            lblMarques.Visible = !visage;
+            cboMarques.Visible = !visage;
+        }
+
+        /// <summary>
+        /// chargement de la fenêtre
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void frmBeautyShop_Load(object sender, EventArgs e)
+        {
+            rdbSoinVisage.Checked = true;
+            changeVisible(true);
+            cboBesoins.SelectedIndex = 0;
+            cboMarques.SelectedIndex = 0;
+            txtLogin.Text = "";
+            txtLogin.Focus();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void rdbSoinVisage_CheckedChanged(object sender, EventArgs e)
+        {
+            changeVisible(true);
+        }
+
+        private void rdbMaquillage_CheckedChanged(object sender, EventArgs e)
+        {
+            changeVisible(false);
+        }
+
+        /// <summary>
+        /// mis à jour la list client
+        /// </summary>
+        private void MisAJourListClient()
+        {
+            // vider la list
+            lstClients.Items.Clear();
+
+            // remplir la list client
+            for (int k = 0; k < nbClient; k++)
+            {
+                lstClients.Items.Add(lesClients[k].info());
+            }
+                
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnAjouter_Click(object sender, EventArgs e)
+        {
+            // ajout un client dans la list
+            nbClient++;
+
+            // si clic sur le bouton radio Soin Visage
+            if (rdbSoinVisage.Checked)
+            {
+                // si le text dans login est vidé (inconnu)
+                if (txtLogin.Text == "") 
+                {
+                    lesClients[nbClient - 1] = new SoinVisage(cboBesoins.SelectedItem.ToString());
+                }
+                else
+                {
+                    lesClients[nbClient - 1] = new SoinVisage(txtLogin.Text.ToUpper(), cboBesoins.SelectedItem.ToString(), (int)nudQuantités.Value);
+                }
+            }
+            else
+            {
+                // si le text dans login est vidé (inconnu)
+                if (txtLogin.Text == "")
+                {
+                    lesClients[nbClient - 1] = new Maquillage(cboMarques.SelectedItem.ToString());
+                }
+                else
+                {
+                    lesClients[nbClient - 1] = new Maquillage(txtLogin.Text.ToUpper(), cboMarques.SelectedItem.ToString(), (int)nudQuantités.Value);
+                }
+            }
+            // mis à jour la list
+            MisAJourListClient();
+
+            // vider le login et reposition sur le login
+            txtLogin.Text = "";
+            txtLogin.Focus();
+        }
+    }
+}
